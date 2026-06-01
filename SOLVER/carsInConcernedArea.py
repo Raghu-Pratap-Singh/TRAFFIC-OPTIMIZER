360e0049252f9d8b1c1e9c30285cf9f07d201be6
Raghu-Pratap-Singh <raghupratapsinghparmar@gmail.com>
2026-06-02 00:25:21 +0530

    EXAMPLE DONE

---
+++ SOLVER/carsInConcernedArea.py
@@ -0,0 +1,34 @@
+cars = [
+    {"id": 1, "node": 2, "destination": 29},
+    {"id": 2, "node": 5, "destination": 24},
+    {"id": 3, "node": 7, "destination": 20},
+    {"id": 4, "node": 8, "destination": 27},
+    {"id": 5, "node": 11, "destination": 25},
+
+    {"id": 6, "node": 12, "destination": 28},
+    {"id": 7, "node": 13, "destination": 21},
+    {"id": 8, "node": 14, "destination": 26},
+    {"id": 9, "node": 15, "destination": 24},
+    {"id": 10, "node": 16, "destination": 29},
+
+    {"id": 11, "node": 17, "destination": 0},
+    {"id": 12, "node": 18, "destination": 5},
+    {"id": 13, "node": 19, "destination": 1},
+    {"id": 14, "node": 20, "destination": 4},
+    {"id": 15, "node": 21, "destination": 8},
+
+    {"id": 16, "node": 22, "destination": 3},
+    {"id": 17, "node": 23, "destination": 6},
+    {"id": 18, "node": 24, "destination": 10},
+    {"id": 19, "node": 25, "destination": 11},
+    {"id": 20, "node": 26, "destination": 12},
+
+    {"id": 21, "node": 27, "destination": 7},
+    {"id": 22, "node": 28, "destination": 13},
+    {"id": 23, "node": 29, "destination": 15},
+    {"id": 24, "node": 9, "destination": 22},
+    {"id": 25, "node": 6, "destination": 18},
+]
+
+# accident location
+accident_node = 18
